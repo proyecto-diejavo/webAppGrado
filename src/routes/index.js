1bd424440cfa264dcc6cf362fcfc879be6136f6a
Javier Reyes Cantor <jreyes@webnet.com.co>
2018-11-15 23:36:04 -0500

    New components admin, users and edit front login

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,8 @@ import BartenderMainRoute from './bartenderMain'
 import adminMainRoute from './adminMain'
 import usersRoute from './users'
 import WaiterTablesRoute from './waiterTables'
+import adminMainRoute from './adminMain'
+import usersRoute from './users'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
@@ -26,6 +28,8 @@ export const createRoutes = store => ({
     adminMainRoute(store),
     usersRoute(store),
     WaiterTablesRoute(store),
+    adminMainRoute(store),
+    usersRoute(store),
     // AsyncRoute(store) // async routes setup by passing store
     // SyncRoute, // sync routes just need route object by itself
     /* Place all Routes above here so NotFoundRoute can act as a 404 page */
